004c35affb1b3378b95d0875b900315805e00fe2
Antonio Tejada <antoniotejada@MacBook-Pro-de-Antonio.local>
2017-07-15 14:44:18 +0200

    First commit, Hello Kotlin

---
+++ app/src/main/java/es/antoniotejada/kweatherapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package es.antoniotejada.kweatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by apptodev on 6/7/17.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
