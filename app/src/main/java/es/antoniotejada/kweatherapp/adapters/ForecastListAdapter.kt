b18b4f13cc0168b6fe04afe620da957727e2928c
Antonio Tejada <antoniotejada@MacBook-Pro-de-Antonio.local>
2017-07-17 20:29:13 +0200

    Simple forecast list adapter

---
+++ app/src/main/java/es/antoniotejada/kweatherapp/adapters/ForecastListAdapter.kt
@@ -0,0 +1,23 @@
+package es.antoniotejada.kweatherapp.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by apptodev on 17/7/17.
+ */
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
